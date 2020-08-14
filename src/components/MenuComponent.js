a554edd0cd010e7a74b8f07ed208768531168ce6
z-sohaib <js_zouambia@esi.dz>
2020-08-14 12:37:42 +0100

    Lifecycle Methods Part 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -15,6 +15,11 @@ class Menu extends Component {
     this.state = {
       selectedDish: null,
     };
+    console.log("Menu Component Constructor Is Invoked");
+  }
+
+  componentDidMount() {
+    console.log("Menu Component componentDidMount Is Invoked");
   }
 
   onDishSelect(dish) {
@@ -50,6 +55,8 @@ class Menu extends Component {
         </div>
       );
     });
+
+    console.log("Menu Component Render Is Invoked");
 
     return (
       <div className="container">
